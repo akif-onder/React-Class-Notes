9598d1fdce1a09a74acdcd793ee78904ea6a6844
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-29 15:55:30 +0300

    form added

---
+++ rc06_forms_otherEvents/forms_otherevents/src/components/1-forms/Form.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+const handleSubmit = () => {
+    alert(` username:${username}
+    email:${email}
+    password:${password}`       
+    );
+};
+
+  return (
+    <div>
+      <form style={{ margin: "5rem" }} onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username <span className="text-danger">{username}</span>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            value={username}
+            onChange={handleUsername}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address <span className="text-danger">{email}</span>
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            
+          />
+        </div>
+        
+        <button type="submit" className="btn btn-primary" >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
