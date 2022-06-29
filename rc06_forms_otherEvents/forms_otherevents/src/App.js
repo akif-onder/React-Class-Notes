fa3af666abaf5f9e1be7d3abfb41156ae6dcac94
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-29 19:03:02 +0300

    mouse events completed

--- rc06_forms_otherEvents/forms_otherevents/src/App.js
+++ rc06_forms_otherEvents/forms_otherevents/src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Form from "./components/1-forms/Form";
 import FormObject from "./components/1-forms/FormObjects";
 import KeyboardClipboard from "./components/2-keyboardClipbooardEvents/KeyboardClipboard";
+import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
 function App() {
   return (
     <div className="container mt-4 text-center" >
       {/* <Form /> */}
       {/* <FormObject/> */}
-      <KeyboardClipboard/>
+      {/* <KeyboardClipboard/> */}
+      <MouseEvent/>
     </div>
   );
 }
