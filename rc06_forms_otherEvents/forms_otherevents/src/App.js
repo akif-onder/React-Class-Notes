0915fd2e35e4ee1088cf4cd7b4afeb2ffd2d2341
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-29 16:42:02 +0300

    formObject completed

--- rc06_forms_otherEvents/forms_otherevents/src/App.js
+++ rc06_forms_otherEvents/forms_otherevents/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/1-forms/Form";
+import FormObject from "./components/1-forms/FormObjects";
 
 function App() {
   return (
     <div className="container mt-4 text-center" >
-      <Form />
+      {/* <Form /> */}
+      <FormObject/>
     </div>
   );
 }
