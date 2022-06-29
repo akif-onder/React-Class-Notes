86a3adcfe8cad605f0ab1e885b50618d8df41315
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-06-29 18:10:07 +0300

    keyboardEvents concluded

--- rc06_forms_otherEvents/forms_otherevents/src/components/2-keyboardClipbooardEvents/KeyboardClipboard.jsx
+++ rc06_forms_otherEvents/forms_otherevents/src/components/2-keyboardClipbooardEvents/KeyboardClipboard.jsx
@@ -2,9 +2,23 @@ import { useState } from "react";
 
 const KeyboardClipboard = () => {
   const [inputValue, setInputValue] = useState("");
-  const handleKeyDown = (e) =>{
+  const handleKeyDown = (e) => {
     e.target.value = e.target.value.toUpperCase();
-    e.keyCode === 13 && alert('Enter key pressed...');
+    e.keyCode === 13 && alert("Enter key pressed...");
+  };
+  const handleAreaPaste = (e) => {
+    e.target.value += e.clipboardData.getData("text").toLowerCase();
+    e.target.style.border = "3px solid red";
+    e.target.style.backgroundColor = 'lightgreen'
+    e.preventDefault();
+
+  };
+
+  const handleAreaChange = (e) => {
+    if (!e.target.value) {
+        e.target.style.border = '1px solid black'
+        e.target.style.backgroundColor = "transparent"
+    }    
   }
 
   return (
@@ -15,9 +29,17 @@ const KeyboardClipboard = () => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={handleKeyDown}
-        
       />
-      <p className="text-start m-4 text-danger">{inputValue}</p>
+      <p className="text-start m-4 text-danger"
+      onCopy={(e)=>{e.target.style.backgroundColor = "lightblue"}}>{inputValue}</p>
+      <textarea
+        name="area"
+        id="area"
+        cols="50"
+        rows="10"
+        onPaste={handleAreaPaste}
+        onChange={handleAreaChange}
+      ></textarea>
     </div>
   );
 };
